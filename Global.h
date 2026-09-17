cde2a3f113eecd1eaa37bc5c424d8e57a7de6ff9
Yousef <yousef.moh.salateen@gmail.com>
2026-09-18 00:11:22 +0300

    Add clsScreen base class and update Global.h managers
    Added clsScreen.h with base class and header drawing method.
    Updated Global.h to include manager headers, global instances,
    and enPermissions enum. Modified project file to include new header.

--- Global.h
+++ Global.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "Middle Layer/clsClientManager.h"
+#include "Middle Layer/clsUserManager.h"
+
 enum enPermissions : short {
 	eAll = -1, eListClients = 1, eAddNewClient = 2, eDeleteClient = 4, eUpdateClient = 8,
 	eFindClient = 16, eTransactions = 32, eManageUsers = 64
 };
+
+clsClientManager Clients;
+clsUserManager Users;
---
+++ UI Layer/clsScreen.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../Global.h"
+#include <iostream>
+#include <iomanip>
+
+class clsScreen
+{
+protected:
+	static void _DrawScreenHeader(const std::string& Title, const std::string& SubTitle = "")
+	{
+		std::cout << std::setw(37) << std::left << "" << "_________________________\n\n";
+		std::cout << std::setw(37) << std::left << "" << Title << "\n";
+		if (!SubTitle.empty())
+		{
+			std::cout << std::setw(37) << std::left << "" << SubTitle << "\n";
+		}
+		std::cout << std::setw(37) << std::left << "" << "_________________________\n\n";
+	}
+};
+
