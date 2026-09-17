677db0832fa801a2fbd24387bfff9fb4db7691ec
Yousef <yousef.moh.salateen@gmail.com>
2026-09-18 00:43:59 +0300

    Remove C11Bank.rc; update clsSession with user handling
    Deleted C11Bank.rc and its reference from C11Bank.vcxproj.
    Refactored clsSession.h: added includes, introduced clsSession class with _CurrentUser, and implemented constructor/destructor for user initialization and reset.

--- Middle Layer/clsSession.h
+++ Middle Layer/clsSession.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include "clsUser.h"
+#include "../Global.h"
+
+//#include "../UI Layer/clsLoginScreen.h"
+
 class clsSession
 {
+private:
+	clsUser _CurrentUser;
 
+public:
+	clsSession(const std::string& Username, const std::string& Password)
+		: _CurrentUser(Users.Find(Username, Password))
+	{
+		
+	}
+
+	~clsSession()
+	{
+		_CurrentUser = Users.Find("", "");
+	}
 };
 
